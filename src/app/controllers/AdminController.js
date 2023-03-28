ed3a0d48fd50430c7273546f5e39e92e363d372e
Anvq2003 <97084643+Anvq2003@users.noreply.github.com>
2023-03-28 21:16:43 +0700

    Build UI admin

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -6,6 +6,18 @@ class AdminController {
     });
   }
 
+  productList(req, res, next) {
+    res.render('admin/product/list', {
+      layout: 'admin',
+    });
+  }
+
+  productAdd(req, res, next) {
+    res.render('admin/product/add', {
+      layout: 'admin',
+    });
+  }
+
   // [GET] admin/login
 }
 module.exports = new AdminController();
--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const adminController = require('../app/controllers/AdminController');
 
+router.get('/add-product', adminController.productAdd);
+router.get('/products', adminController.productList);
 router.get('/', adminController.index);
 
 module.exports = router;
